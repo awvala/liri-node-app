772612b7dbd82952232e8e418e073e5bf263aeb8
Andrew Vala <38055092+awvala@users.noreply.github.com>
2018-07-12 21:48:13 -0500

    added switch statement

--- liri.js
+++ liri.js
@@ -1,31 +1,66 @@
+// Require the following NPM modules
 require("dotenv").config();
-
 var keys = require("./keys.js");
 var Twitter = require('twitter');
 //var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
 
-//var command = process.argv;
-//var liriCommand = command[2];
-//var liriArgument = command[3];
+// Global Variables
+var command = process.argv;
+var liriCommand = command[2];
+var liriArgument = command[3];
 
+// Node functions
 function getTweets() {
-var Tparams = {
-    screen_name: 'LiriTest7181',
-    count: 20,
-  }
-
-// Twitter requests
-client.get('statuses/user_timeline', Tparams , function (err, tweet, result) {
-    if (err) {
-        console.log(err);
-    } else {
-        for (var i = 0; i <tweet.length; i ++) {
-            console.log("\n" + "Created: " + tweet[i].created_at + "\n" + "Brilliant Observation:  " + tweet[i].text);
-            console.log("==============");
-        }
+    var Tparams = {
+        screen_name: 'LiriTest7181',
+        count: 20,
     }
-});
+
+    // Twitter requests
+    client.get('statuses/user_timeline', Tparams , function (err, tweet, result) {
+        if (err) {
+            console.log(err);
+        } else {
+            for (var i = 0; i <tweet.length; i ++) {
+                console.log("\n" + "Created: " + tweet[i].created_at + "\n" + "Brilliant Observation:  " + tweet[i].text + "\n");
+                console.log("==============");
+            }
+        }
+    });
 }
 
-getTweets();
+/*function getMusic() {
+    var Tparams = {
+        screen_name: 'LiriTest7181',
+        count: 20,
+      }
+    
+    // Twitter requests
+    client.get('statuses/user_timeline', Tparams , function (err, tweet, result) {
+        if (err) {
+            console.log(err);
+        } else {
+            for (var i = 0; i <tweet.length; i ++) {
+                console.log("\n" + "Created: " + tweet[i].created_at + "\n" + "Brilliant Observation:  " + tweet[i].text + "\n");
+                console.log("==============");
+            }
+        }
+    });
+}*/
+
+// Determine which function to run
+switch (liriCommand){
+    case "my-tweets":
+        getTweets();
+        break;
+    case "spotify-this-song":
+        getMusic(liriArgument);
+        break;
+    case "movie-this":
+        getMovie(liriArgument);
+        break;
+    case "do-what-it-says":
+        dothething();
+        break;
+};
