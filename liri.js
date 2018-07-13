72a09e715ee1f4f46e40569c82f9e6536a41a24c
Andrew Vala <38055092+awvala@users.noreply.github.com>
2018-07-12 21:13:54 -0500

    twitter data added

--- liri.js
+++ liri.js
@@ -1,5 +1,33 @@
-require("dotenv").config();
+//require("dotenv").config();
 
-var keys = require("keys.js");
-var spotify = new Spotify(keys.spotify);
-var client = new Twitter(keys.twitter);
+var keys = require("./keys.js");
+var twitter = require('twitter');
+//var spotify = new Spotify(keys.spotify);
+var client = new twitter(keys.twitter);
+
+var command = process.argv;
+var liriCommand = command[2];
+var liriArgument = command[3];
+
+function getTweets() {
+var Tparams = {
+    screen_name: 'LiriTest718',
+    count: 20,
+  }
+
+// Twitter requests
+client.get('statuses/user_timeline', Tparams , function (err, tweet, result) {
+    if (err) {
+        console.log(err);
+    } else {
+        for (var i = 0; i <tweets.length; i ++) {
+            console.log("Created: " + tweet[i].created_at + "\n" + "Brilliant Observation:  " + tweet[i].text + "\n");
+            console.log("==============");
+        }
+    }
+});
+}
+
+getTweets();
+
+
